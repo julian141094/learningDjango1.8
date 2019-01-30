ff76ca6fce3b5694667a938fcc7b7b3ab13dea72
julian141094 <julian.arkangel@gmail.com>
2019-01-30 00:18:13 -0400

    Creacion de primer modelo, y modificacion de la busqueda de las vistas

--- src/newsletter/models.py
+++ src/newsletter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length=120, blank=True, null=True)
+    timestamp = models.TimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self): #Pyton 3.3 is __str__
+        return self.email
